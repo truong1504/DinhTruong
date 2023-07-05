f3c75f961f4686a6ab23dc1701a660f7b598f0bf
truong1504 <125253914+truong1504@users.noreply.github.com>
2023-07-05 14:39:45 +0700

    Add files via upload

---
+++ bai3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int thang;
+	cin >> thang;
+	switch(thang){
+		case 1:
+			cout << "31 ngay";
+			break;
+		case 2:
+			cout << "28 hoac 29 ngay";
+			break;
+		case 3:
+			cout << "31 ngay";
+			break;
+		case 4:
+			cout << "30 ngay";
+			break;
+		case 5:
+			cout << "31 ngay";
+			break;
+		case 6:
+			cout << "30 ngay";
+			break;
+		case 7:
+			cout << "31 ngay";
+			break;
+		case 8:
+			cout << "31 ngay";
+			break;
+		case 9:
+			cout << "30 ngay";
+			break;
+		case 10:
+			cout << "31 ngay";
+			break;
+		case 11:
+			cout << "30 ngay";
+			break;
+		case 12:
+			cout << "31 ngay";
+			break;
+	}
+}
